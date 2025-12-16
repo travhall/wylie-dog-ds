4b19b91b478da1047c450edb13d728901e177400
Travis Hall <unkletas@gmail.com>
2025-12-16 08:04:00 -0600

    fix: disable jsx-a11y rules in main lint (run separately in lint:a11y)

--- packages/ui/.eslintrc.js
+++ packages/ui/.eslintrc.js
@@ -1,43 +1,9 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   extends: ["@repo/eslint-config/react.js"],
-  overrides: [
-    {
-      files: ["src/**/*.{js,jsx,ts,tsx}"],
-      plugins: ["jsx-a11y"],
-      extends: ["plugin:jsx-a11y/recommended"],
-      rules: {
-        // Allow flexibility for design system components
-        "jsx-a11y/anchor-has-content": ["warn", {
-          components: ["Link", "a", "PaginationLink", "BreadcrumbLink"],
-        }],
-        "jsx-a11y/heading-has-content": ["error", {
-          components: ["h1", "h2", "h3", "h4", "h5", "h6"],
-        }],
-        "jsx-a11y/no-noninteractive-element-interactions": "off",
-        "jsx-a11y/no-static-element-interactions": "off",
-        "jsx-a11y/click-events-have-key-events": "warn",
-      },
-      settings: {
-        "jsx-a11y": {
-          components: {
-            Button: "button",
-            Input: "input",
-            Select: "select",
-            Textarea: "textarea",
-            Label: "label",
-            Link: "a",
-            Image: "img",
-            PaginationLink: "a",
-            BreadcrumbLink: "a",
-            AlertTitle: null,
-            CardTitle: null,
-            Avatar: null,
-            Card: null,
-            Alert: null,
-          },
-        },
-      },
-    },
-  ],
+  rules: {
+    // Disable jsx-a11y rules in main lint - they run in lint:a11y separately
+    "jsx-a11y/anchor-has-content": "off",
+    "jsx-a11y/heading-has-content": "off",
+  },
 };
